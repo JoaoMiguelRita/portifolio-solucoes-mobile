d0b0abaf29f7eff30c99f8d15dfe7b383a9d91e7
joao.334523 <joaomiguelrita01@gmail.com>
2026-03-16 19:40:21 -0300

    pedra papel tesoura basico

--- exercicio02/pedraPapelTesoura/app/src/main/java/com/example/pedrapapeltesoura/MainActivity.kt
+++ exercicio02/pedraPapelTesoura/app/src/main/java/com/example/pedrapapeltesoura/MainActivity.kt
@@ -2,18 +2,18 @@ package com.example.pedrapapeltesoura
 
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,12 +22,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.pedrapapeltesoura.ui.theme.PedraPapelTesouraTheme
-import kotlinx.coroutines.withTimeout
-import kotlin.concurrent.timer
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,8 +39,13 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun TelaHome() {
         var result by remember { mutableStateOf("") }
-        val opcoes = listOf("Pedra", "Papel", "Tesoura")
+        val opcoes = listOf(
+            R.drawable.pedra,
+            R.drawable.papel,
+            R.drawable.tesoura
+        )
         val randomOpion = opcoes.random()
+        var imagem by remember { mutableStateOf<Int?>(null) }
         var tempo by remember { mutableStateOf(3) }
         val timer = object : CountDownTimer(4000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
@@ -61,24 +63,33 @@ class MainActivity : ComponentActivity() {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            Text("Vamos jogar pedra, papel, tesoura?")
-            Button(onClick = {
-                timer.start()
-                result = "Eu escolho: $randomOpion"
-            }) {
-                Text("Jogar")
-            }
-
             Row(
                 modifier = Modifier
                     .padding(16.dp)
                     .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Absolute.Center)
+            {Text("Vamos jogar pedra, papel, tesoura?")}
+            Button(onClick = {
+                timer.start()
+                imagem = randomOpion
+            }) {
+                Text("Jogar")
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
                 horizontalArrangement = Arrangement.Absolute.Center
             ) {
-
-                if (tempo == 0) {
+                if (tempo == 0 && imagem != null) {
                     Text("$result")
-                } else {
+                    imagem?.let {
+                        Image(
+                            painter = painterResource(id = it),
+                            contentDescription = "Escolha",
+                            modifier = Modifier.size(150.dp)
+                        )
+                    }
+                } else if (imagem != null) {
                     Text("$tempo")
                 }
             }
